f0b6952a9ad22352be6fd30e081c8aba5a7a6ace
scriptinfo24 <scriptinfo24@gmail.com>
2024-12-15 14:16:01 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+# module_4_1.py
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+print(result1)  # Ожидается: 23.0
+print(result2)  # Ожидается: 'Ошибка'
+print(result3)  # Ожидается: 7.0
+print(result4)  # Ожидается: inf
